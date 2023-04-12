632222d449edf1793f84330e23270b004defca62
burty7120 <124809701+burty7120@users.noreply.github.com>
2023-04-13 00:34:50 +0300

    First commit
    First commit

---
+++ Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int num = 10;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
